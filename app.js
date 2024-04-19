3b5f44364d121c7a8332bbd3c72bdd49cd4d0912
Stanley Mtonyole <stanleymtonyole@gmail.com>
2024-04-19 17:31:06 +0300

    Updates

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.get('/increment/:startNumber', (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT_3 || 5003;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
